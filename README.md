6288c7c3da53c9b44335dfeca5d1916dfbc1e60f
jashimgit <jashim.ncn@gmail.com>
2022-09-27 09:17:27 +0600

    Go :: Jashim :: syntax of Go added

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ### Let's hug Go
 Date: 29-09-2022
+
+### Topics
+    1. syntax
+    2. comments
+    3. variables
+    4. constants
---
+++ syntax.go
@@ -0,0 +1,26 @@
+// this line is mandatory for
+// any go file
+package main
+
+// this line is used to import
+// any package e.g: fmt is a package of Golang
+
+import "fmt"
+
+// this function is mandatory for
+// each go file
+/*
+	this is a multi line comment example
+	func is a reserved keyword of Go, means function
+	main is the main function of Go.
+
+	type: go run filename.go to run any go file
+	in your terminal
+	
+	note: any string should be written within double qoute,
+	single will not work here
+*/
+
+func main() {
+	fmt.Println("Welcome to Golang")
+}
